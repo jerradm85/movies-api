71a0971710a12b3a2e47ddb6a80fd8fcca293a3d
Your name here <kr.shampa@yahoo.com>
2020-07-03 15:48:09 -0500

    fixing Api

--- server.js
+++ server.js
@@ -13,20 +13,18 @@ app.use(helmet())
 
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_KEY
-    const authToken = req.get('Authorization')
-  
+    console.log(apiToken);
+    const authToken = req.get('Authorization')  
+    console.log(authToken)
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
       return res.status(401).json({ error: 'Unauthorized request. Provide a proper authorization key' })
-    }
+    } 
     // move to the next middleware
     next()
   })
-  
-
-
 function handleMovies(req, res) {
     let response = MOVIES;
-    console.log(response)
+    //console.log(response)
   // filter our pokemon by name if name query param is present
   if (req.query.country) {
     response = response.filter(movieCountry =>
